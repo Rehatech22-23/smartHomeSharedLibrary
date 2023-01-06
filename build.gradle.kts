8f6dc9edd04afe0d7f1786c60773743d46da93e7
Sebastian <1sebastian1@online.de>
2023-01-06 16:59:53 +0100

    update publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    id("maven-publish")
+    id("java")
 
 }
 
@@ -23,5 +25,15 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+publishing{
+    publications {
+        create<MavenPublication>("maven"){
+            groupId = "de.rehatech"
+            artifactId ="homeekt"
+            version = "0.0.1"
+            from(components["java"])
+        }
+    }
 }
 
