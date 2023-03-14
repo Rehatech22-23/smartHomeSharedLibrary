1ca10f111d002d46efceee8b31a66ea6b3f1aeed
Fynn Debus <fynn.debus@adesso.de>
2023-03-14 12:44:54 +0100

    changed triggerType to comparisonType

--- src/main/kotlin/de/rehatech2223/datamodel/RoutineDTO.kt
+++ src/main/kotlin/de/rehatech2223/datamodel/RoutineDTO.kt
@@ -5,18 +5,22 @@ import de.rehatech2223.datamodel.util.TriggerEventByDeviceDTO
 import de.rehatech2223.datamodel.util.TriggerTimeDTO
 import java.util.*
 
+
+/**
+ * @Param comparisonType: type of comparison (less, greater or equal) <, >, = with 0, 1, 2
+ */
 @kotlinx.serialization.Serializable
 class RoutineDTO private constructor(
     val routineName: String,
     val routineId: Long? = null,
-    val triggerType: Int,
+    val comparisonType: Int,
     val triggerTime: TriggerTimeDTO? = null,
     val triggerEventByDeviceDTO: TriggerEventByDeviceDTO? = null,
     val routineEventDTO: ArrayList<RoutineEventDTO>
 ){
     data class Builder(
         val routineName: String,
-        val triggerType: Int,
+        val comparisonType: Int,
         val routineEventDTO: ArrayList<RoutineEventDTO>,
         var routineId: Long? = null,
         var triggerTime: TriggerTimeDTO? = null,
@@ -25,7 +29,7 @@ class RoutineDTO private constructor(
         fun routineId(routineId: Long) = apply { this.routineId = routineId }
         fun triggerTime(triggerTime: TriggerTimeDTO) = apply { this.triggerTime = triggerTime }
         fun triggerEventByDeviceDTO(triggerEventByDeviceDTO: TriggerEventByDeviceDTO) = apply { this.triggerEventByDeviceDTO = triggerEventByDeviceDTO }
-        fun build() = RoutineDTO(routineName, routineId, triggerType, triggerTime, triggerEventByDeviceDTO, routineEventDTO)
+        fun build() = RoutineDTO(routineName, routineId, comparisonType, triggerTime, triggerEventByDeviceDTO, routineEventDTO)
     }
 }
 
